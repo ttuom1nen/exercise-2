a67ede288b85fdf1ed88022c74eff3c3ddcd39a5
ttuom1nen <ttuominen@outlook.com>
2019-05-25 13:16:22 +0300

    Styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,9 +3,14 @@ import React from "react";
 class SearchBar extends React.Component {
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" />
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>
+              Image Search
+              <input type="text" />
+            </label>
+          </div>
         </form>
       </div>
     );
