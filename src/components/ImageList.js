0fb584d64681fc14af0fcff661aefe4ba6d267d9
ttuom1nen <ttuominen@outlook.com>
2019-05-30 22:37:36 +0300

    List styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,11 @@
+import "../css/ImageList.css";
 import React from "react";
 
 const ImageList = props => {
-  const images = props.images.map(image => {
-    return (
-      <img key={image.id} src={image.urls.small} alt={image.description} />
-    );
+  const images = props.images.map(({ description, id, urls }) => {
+    return <img key={id} src={urls.small} alt={description} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
